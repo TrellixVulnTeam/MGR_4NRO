f33866da4f371917c24834e945c65e6b90d28bb5
piotr.onyszczuk1 <piotr.onyszczuk1@gmail.com>
2021-06-12 15:00:35 +0200

    Poprawki

--- sem1/GK2/proj3/Teselacja/Teselacja/tessellatedBezierHS.hlsl
+++ sem1/GK2/proj3/Teselacja/Teselacja/tessellatedBezierHS.hlsl
@@ -54,21 +54,52 @@ HSPatchOutput HS_Patch(InputPatch<HSInput, INPUT_PATCH_SIZE> ip, uint patchId : 
 {
 	HSPatchOutput o;
 	float factor = 1.0f;
+	float factorEdgeL=1.0f;
+	float factorEdgeR=1.0f;
+	float factorEdgeU=1.0f;
+	float factorEdgeD=1.0f;
 	if (parameters.useLOD != 0)
 	{
 		float4 somePoint = ip[0].pos + ip[5].pos + ip[10].pos + ip[15].pos;
 		somePoint /= 4.0f;
 		somePoint = mul(viewMatrix, somePoint);
 		factor = factor_f(somePoint.z);
+
+		float4 dPoint = ip[5].pos;
+		dPoint = mul(viewMatrix, dPoint);
+		factorEdgeD = factor_f(dPoint.z);
+
+		float4 uPoint = ip[9].pos;
+		uPoint = mul(viewMatrix, uPoint);
+		factorEdgeU = factor_f(uPoint.z);
+
+		float4 lPoint = ip[5].pos;
+		lPoint = mul(viewMatrix, lPoint);
+		factorEdgeL = factor_f(lPoint.z);
+
+		float4 rPoint = ip[6].pos;
+		rPoint = mul(viewMatrix, rPoint);
+		factorEdgeR = factor_f(rPoint.z);
+
 	}
 	
-	float edgeTessFactor = factor * parameters.edgeTessFactor;
-	if (edgeTessFactor < 1.0f)edgeTessFactor = 1.0f;
+	float edgeTessFactorL = factorEdgeL * parameters.edgeTessFactor;
+	float edgeTessFactorR = factorEdgeR * parameters.edgeTessFactor;
+	float edgeTessFactorU = factorEdgeU * parameters.edgeTessFactor;
+	float edgeTessFactorD = factorEdgeD * parameters.edgeTessFactor;
+	if (edgeTessFactorL < 1.0f)edgeTessFactorL = 1.0f;
+	if (edgeTessFactorR < 1.0f)edgeTessFactorR = 1.0f;
+	if (edgeTessFactorU < 1.0f)edgeTessFactorU = 1.0f;
+	if (edgeTessFactorD < 1.0f)edgeTessFactorD = 1.0f;
 
 	float insideTessFactor = factor * parameters.insideTessFactor;
 	if (insideTessFactor < 1.0f)insideTessFactor = 1.0f;
 	
-	o.edges[0] = o.edges[1] = o.edges[2] = o.edges[3] = edgeTessFactor;
+	//o.edges[0] = o.edges[1] = o.edges[2] = o.edges[3] = edgeTessFactor;
+	o.edges[0] = edgeTessFactorL;
+	o.edges[1] = edgeTessFactorD;
+	o.edges[2] = edgeTessFactorR;
+	o.edges[3] = edgeTessFactorU;
 	o.inside[0] = o.inside[1] = insideTessFactor;
 	return o;
 }
