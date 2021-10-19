34d648c481adbb269a6ba05b75ceb8bad1a4f73a
piotr.onyszczuk1 <piotr.onyszczuk1@gmail.com>
2021-10-19 15:19:12 +0200

    Sprezyna

---
+++ sem2/Symulacje/Sprezyna/Sprezyna/Form1.Designer.cs
@@ -0,0 +1,179 @@
+﻿namespace Sprezyna {
+    partial class Form1 {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent() {
+            this.chartControl1 = new DevExpress.XtraCharts.ChartControl();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.tableLayoutPanel2 = new System.Windows.Forms.TableLayoutPanel();
+            this.chartControl2 = new DevExpress.XtraCharts.ChartControl();
+            this.chartControl3 = new DevExpress.XtraCharts.ChartControl();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.chartControl1)).BeginInit();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.tableLayoutPanel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.chartControl2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chartControl3)).BeginInit();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // chartControl1
+            // 
+            this.chartControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.chartControl1.Legend.Name = "Default Legend";
+            this.chartControl1.Location = new System.Drawing.Point(3, 3);
+            this.chartControl1.Name = "chartControl1";
+            this.chartControl1.SeriesSerializable = new DevExpress.XtraCharts.Series[0];
+            this.chartControl1.Size = new System.Drawing.Size(1129, 153);
+            this.chartControl1.TabIndex = 0;
+            this.chartControl1.Click += new System.EventHandler(this.chartControl1_Click);
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 2;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 10F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 90F));
+            this.tableLayoutPanel1.Controls.Add(this.panel1, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.tableLayoutPanel2, 1, 0);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 1;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(1267, 644);
+            this.tableLayoutPanel1.TabIndex = 1;
+            // 
+            // panel1
+            // 
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(3, 3);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(120, 638);
+            this.panel1.TabIndex = 1;
+            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            // 
+            // tableLayoutPanel2
+            // 
+            this.tableLayoutPanel2.ColumnCount = 1;
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel2.Controls.Add(this.chartControl1, 0, 0);
+            this.tableLayoutPanel2.Controls.Add(this.chartControl2, 0, 1);
+            this.tableLayoutPanel2.Controls.Add(this.chartControl3, 0, 2);
+            this.tableLayoutPanel2.Controls.Add(this.panel2, 0, 3);
+            this.tableLayoutPanel2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel2.Location = new System.Drawing.Point(129, 3);
+            this.tableLayoutPanel2.Name = "tableLayoutPanel2";
+            this.tableLayoutPanel2.RowCount = 4;
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanel2.Size = new System.Drawing.Size(1135, 638);
+            this.tableLayoutPanel2.TabIndex = 2;
+            // 
+            // chartControl2
+            // 
+            this.chartControl2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.chartControl2.Legend.Name = "Default Legend";
+            this.chartControl2.Location = new System.Drawing.Point(3, 162);
+            this.chartControl2.Name = "chartControl2";
+            this.chartControl2.SeriesSerializable = new DevExpress.XtraCharts.Series[0];
+            this.chartControl2.Size = new System.Drawing.Size(1129, 153);
+            this.chartControl2.TabIndex = 1;
+            // 
+            // chartControl3
+            // 
+            this.chartControl3.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.chartControl3.Legend.Name = "Default Legend";
+            this.chartControl3.Location = new System.Drawing.Point(3, 321);
+            this.chartControl3.Name = "chartControl3";
+            this.chartControl3.SeriesSerializable = new DevExpress.XtraCharts.Series[0];
+            this.chartControl3.Size = new System.Drawing.Size(1129, 153);
+            this.chartControl3.TabIndex = 2;
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.button2);
+            this.panel2.Controls.Add(this.button1);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel2.Location = new System.Drawing.Point(3, 480);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(1129, 155);
+            this.panel2.TabIndex = 3;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(17, 21);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Start";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(17, 76);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Pause";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1267, 644);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.chartControl1)).EndInit();
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.chartControl2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chartControl3)).EndInit();
+            this.panel2.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private DevExpress.XtraCharts.ChartControl chartControl1;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel2;
+        private DevExpress.XtraCharts.ChartControl chartControl2;
+        private DevExpress.XtraCharts.ChartControl chartControl3;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ sem2/Symulacje/Sprezyna/Sprezyna/Form1.cs
@@ -0,0 +1,188 @@
+﻿using DevExpress.Utils;
+using DevExpress.XtraCharts;
+using System;
+using System.Collections.ObjectModel;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace Sprezyna
+{
+    public partial class Form1 : Form
+    {
+        const int ViewportPointCount = 100;
+        ObservableCollection<DataPoint> positionPoints = new ObservableCollection<DataPoint>();
+        ObservableCollection<DataPoint> velocityPoints = new ObservableCollection<DataPoint>();
+        ObservableCollection<DataPoint> accelerationPoints = new ObservableCollection<DataPoint>();
+        System.Windows.Forms.Timer timer;
+        public Form1() { InitializeComponent(); }
+
+        void Form1_Load(object sender, EventArgs e)
+        {
+            #region chart1
+            {
+                chartControl1.Titles.Add(new ChartTitle { Text = "Position" });
+
+                Series series = new Series();
+                series.ChangeView(ViewType.Point);
+                series.DataSource = positionPoints;
+                series.DataSourceSorted = true;
+                series.ArgumentDataMember = "Argument";
+                series.ValueDataMembers.AddRange("Value");
+                chartControl1.Series.Add(series);
+
+                PointSeriesView seriesView = (PointSeriesView)series.View;
+                seriesView.LastPoint.LabelDisplayMode = SidePointDisplayMode.DiagramEdge;
+                seriesView.LastPoint.Label.TextPattern = "{V:f2}";
+                seriesView.AggregateFunction = SeriesAggregateFunction.None;
+
+                XYDiagram diagram = (XYDiagram)chartControl1.Diagram;
+                diagram.AxisX.DateTimeScaleOptions.ScaleMode = ScaleMode.Continuous;
+                diagram.AxisX.Label.ResolveOverlappingOptions.AllowRotate = false;
+                diagram.AxisX.Label.ResolveOverlappingOptions.AllowStagger = false;
+                diagram.AxisX.WholeRange.SideMarginsValue = 0;
+                diagram.DependentAxesYRange = DefaultBoolean.True;
+                diagram.AxisY.WholeRange.AlwaysShowZeroLevel = false;
+            }
+            #endregion
+            #region chart2
+            {
+                chartControl2.Titles.Add(new ChartTitle { Text = "Velocity" });
+
+                Series series = new Series();
+                series.ChangeView(ViewType.Point);
+                series.DataSource = velocityPoints;
+                series.DataSourceSorted = true;
+                series.ArgumentDataMember = "Argument";
+                series.ValueDataMembers.AddRange("Value");
+                chartControl2.Series.Add(series);
+
+                PointSeriesView seriesView = (PointSeriesView)series.View;
+                seriesView.LastPoint.LabelDisplayMode = SidePointDisplayMode.DiagramEdge;
+                seriesView.LastPoint.Label.TextPattern = "{V:f2}";
+                seriesView.AggregateFunction = SeriesAggregateFunction.None;
+
+                XYDiagram diagram = (XYDiagram)chartControl2.Diagram;
+                diagram.AxisX.DateTimeScaleOptions.ScaleMode = ScaleMode.Continuous;
+                diagram.AxisX.Label.ResolveOverlappingOptions.AllowRotate = false;
+                diagram.AxisX.Label.ResolveOverlappingOptions.AllowStagger = false;
+                diagram.AxisX.WholeRange.SideMarginsValue = 0;
+                diagram.DependentAxesYRange = DefaultBoolean.True;
+                diagram.AxisY.WholeRange.AlwaysShowZeroLevel = false;
+            }
+            #endregion            
+            #region chart3
+            {
+                chartControl3.Titles.Add(new ChartTitle { Text = "Acceleration" });
+
+                Series series = new Series();
+                series.ChangeView(ViewType.Point);
+                series.DataSource = accelerationPoints;
+                series.DataSourceSorted = true;
+                series.ArgumentDataMember = "Argument";
+                series.ValueDataMembers.AddRange("Value");
+                chartControl3.Series.Add(series);
+
+                PointSeriesView seriesView = (PointSeriesView)series.View;
+                seriesView.LastPoint.LabelDisplayMode = SidePointDisplayMode.DiagramEdge;
+                seriesView.LastPoint.Label.TextPattern = "{V:f2}";
+                seriesView.AggregateFunction = SeriesAggregateFunction.None;
+
+                XYDiagram diagram = (XYDiagram)chartControl3.Diagram;
+                diagram.AxisX.DateTimeScaleOptions.ScaleMode = ScaleMode.Continuous;
+                diagram.AxisX.Label.ResolveOverlappingOptions.AllowRotate = false;
+                diagram.AxisX.Label.ResolveOverlappingOptions.AllowStagger = false;
+                diagram.AxisX.WholeRange.SideMarginsValue = 0;
+                diagram.DependentAxesYRange = DefaultBoolean.True;
+                diagram.AxisY.WholeRange.AlwaysShowZeroLevel = false;
+            }
+            #endregion
+
+            timer = new System.Windows.Forms.Timer();
+            timer.Interval = 30;
+            timer.Start();
+            timer.Tick += Timer_Tick;
+        }
+        int counter = 0;
+        void Timer_Tick(object sender, EventArgs e)
+        {
+            panel1.Invalidate();
+            positionPoints.Add(new DataPoint(DateTime.Now, x));
+            velocityPoints.Add(new DataPoint(DateTime.Now, v));
+            accelerationPoints.Add(new DataPoint(DateTime.Now, a));
+            if (positionPoints.Count > ViewportPointCount)
+            {
+                positionPoints.RemoveAt(0);
+                velocityPoints.RemoveAt(0);
+                accelerationPoints.RemoveAt(0);
+            }
+        }
+        double GenerateValue(double x)
+        {
+            return Math.Sin(x) * 3 + x / 2 + 5;
+        }
+        private float x = -1.0f, m = 1.0f;
+        private float v = 0.0f, a = 0.0f;
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            timer.Stop();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            timer.Start();
+        }
+
+        private void chartControl1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+            base.OnPaint(e);
+            Panel p = sender as Panel;
+
+            int width = p.Size.Width;
+            int height = p.Size.Height;
+
+            int blockSize = width / 2;
+            int bias = height / 20;
+            int mid = height / 2;
+
+            int amp = mid - (bias + blockSize / 2);
+
+            float k = 0.01f;
+            float F = -k * x;
+            a = F / m;
+            v = v + a;
+            x += v;
+
+            int pos = mid + (int)(x * amp) - blockSize / 2;
+
+            int len = pos - bias / 2;
+
+
+            using (var myPen = new SolidBrush(Color.Aqua))
+            {
+                var area = new Rectangle(new Point(p.Size.Width / 4, pos), new Size(blockSize, blockSize));
+                e.Graphics.FillRectangle(myPen, area);
+            }
+            using (var brush2 = new SolidBrush(Color.Red))
+            {
+                var area2 = new Rectangle(new Point(3 * p.Size.Width / 8, bias / 2), new Size(p.Size.Width / 4, len));
+                e.Graphics.FillRectangle(brush2, area2);
+            }
+        }
+    }
+    public class DataPoint
+    {
+        public DateTime Argument { get; set; }
+        public double Value { get; set; }
+        public DataPoint(DateTime argument, double value)
+        {
+            Argument = argument;
+            Value = value;
+        }
+    }
+}
