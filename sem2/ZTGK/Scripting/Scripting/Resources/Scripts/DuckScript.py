e81052dbb0824d18444e73a15cd2380c39914488
piotr.onyszczuk1 <piotr.onyszczuk1@gmail.com>
2022-01-20 22:08:49 +0100

    Kaczka on da Bezier

--- sem2/ZTGK/Scripting/Scripting/Resources/Scripts/DuckScript.py
+++ sem2/ZTGK/Scripting/Scripting/Resources/Scripts/DuckScript.py
@@ -1,20 +1,62 @@
 from API import Vector3
+from API import Vector2
+from API import Vector4
+from API import Bezier
 import random
 import time
+import math
 
 class DuckScript:
     def __init__(self):
         self.gameObject = None
+        self.dt = 0.005
+        self.t = 0.0
+        self.bezier = Bezier()
+        self.lastRand = Vector2(0.0,0.0)
+        self.bezier.AddControlPoint(self.lastRand)
+        self.NewPoint()
+        self.NewPoint()
+        self.NewPoint()
+        self.pos = self.GetPos()
+        self.lastPos=self.pos
         pass
+
+    def RandPoint(self):
+        return Vector2(random.random(),random.random())
+
+    def Dist(self,v1,v2):
+        x_diff = v1.x-v2.x
+        y_diff = v1.y-v2.y
+        return math.sqrt(x_diff*x_diff+y_diff*y_diff)
+        
+    def NewPoint(self):
+        point = self.RandPoint()
+        while self.Dist(point,self.lastRand)<0.8:
+            point = self.RandPoint()
+        self.lastRand=point
+        self.bezier.AddControlPoint(point)
+        pass
+
+    def GetPos(self):
+        return Vector2(self.bezier.GetXPos(self.t),self.bezier.GetYPos(self.t))
+
+    def ConvertPos(self, pos):
+        scale = 5;
+        translate = -2.5
+        return Vector3(scale * pos.x + translate, 0.0, scale*pos.y+ translate)
 
     def OnCreate(self):
         self.gameObject.SetScale(Vector3(0.01,0.01,0.01))
         pass
 
     def OnUpdate(self):
-        pos = self.gameObject.GetPosition()
-        nextPos = pos + Vector3(0.01,0,0.01)
-        nextPos2 = nextPos + Vector3(0.01,0,0.01)
-        self.gameObject.SetPosition(nextPos)
-        self.gameObject.SetNextPosition(nextPos2)
+        self.t += self.dt
+        if self.t >= 1.0:
+            self.t = 0.0
+            self.NewPoint()
+
+        self.lastPos = self.pos
+        self.pos = self.GetPos()
+        self.gameObject.SetPosition(self.ConvertPos(self.lastPos))
+        self.gameObject.SetNextPosition(self.ConvertPos(self.pos))
         pass
--- sem2/ZTGK/Scripting/Scripting/ScriptAPI.cpp
+++ sem2/ZTGK/Scripting/Scripting/ScriptAPI.cpp
@@ -5,11 +5,70 @@
 
 namespace py = pybind11;
 using namespace duckApp;
+
+class Bezier
+{
+public:
+	Bezier() = default;
+	void AddControlPoint(XMFLOAT2 newPoint)
+	{
+		deBoor0 = deBoor1;
+		deBoor1 = deBoor2;
+		deBoor2 = deBoor3;
+		deBoor3 = newPoint;
+	}
+
+	float GetXPos(float t)
+	{
+		return ResFromBernstein(DeBoorToBernstein(deBoor0.x, deBoor1.x, deBoor2.x, deBoor3.x), t);
+	}
+
+	float GetYPos(float t)
+	{
+		return ResFromBernstein(DeBoorToBernstein(deBoor0.y, deBoor1.y, deBoor2.y, deBoor3.y), t);
+	}
+private:
+	XMFLOAT2 deBoor0, deBoor1, deBoor2, deBoor3;
+	XMFLOAT4 DeBoorToBernstein(float a, float b, float c, float d)
+	{
+		float p1 = a + 2.0f / 3.0f * (b - a);
+		float p2 = b + 1.0f / 3.0f * (c - b);
+		float p3 = b + 2.0f / 3.0f * (c - b);
+		float p4 = c + 1.0f / 3.0f * (d - c);
+
+		XMFLOAT4 bernsteins;
+		bernsteins.x = (p1 + p2) / 2.0f;
+		bernsteins.y = p2;
+		bernsteins.z = p3;
+		bernsteins.w = (p3 + p4) / 2.0f;
+		return bernsteins;
+	}
+	float ResFromBernstein(XMFLOAT4 b, float t)
+	{
+		float coeffs[4];
+		coeffs[0] = b.x;
+		coeffs[1] = b.y;
+		coeffs[2] = b.z;
+		coeffs[3] = b.w;
+		for (int i = 4; i > 0; --i)
+		{
+			for (int j = 0; j < i - 1; ++j)
+			{
+				coeffs[j] = (1 - t) * coeffs[j] + t * coeffs[j + 1];
+			}
+		}
+		return coeffs[0];
+	}
+};
+
 /* TODO (5.02) Create C++ API Module To Be Embedded Into Python */
 PYBIND11_EMBEDDED_MODULE(API, _module)
 {
+#pragma region Vectors 
+
+
 	py::class_<DirectX::XMFLOAT3>(_module, "Vector3")
-		.def(py::init<float,float,float>())
+		.def(py::init<float, float, float>())
 		.def("__repr__", [](const DirectX::XMFLOAT3& vec) {
 		using namespace std::string_literals;
 		return "["s + std::to_string(vec.x) + ","s + std::to_string(vec.y) + ","s + std::to_string(vec.z) + "]"s;
@@ -23,7 +82,7 @@ PYBIND11_EMBEDDED_MODULE(API, _module)
 				DirectX::XMFLOAT3 result;
 				XMStoreFloat3(&result, DirectX::XMVectorMultiply(DirectX::XMLoadFloat3(&vec1), DirectX::XMLoadFloat3(&vec2)));
 				return result;
-			},py::is_operator())
+			}, py::is_operator())
 		.def("__add__",
 			[](DirectX::XMFLOAT3& vec1, DirectX::XMFLOAT3& vec2)
 			{
@@ -31,6 +90,62 @@ PYBIND11_EMBEDDED_MODULE(API, _module)
 				XMStoreFloat3(&result, DirectX::XMVectorAdd(DirectX::XMLoadFloat3(&vec1), DirectX::XMLoadFloat3(&vec2)));
 				return result;
 			}, py::is_operator());
+
+	py::class_<DirectX::XMFLOAT2>(_module, "Vector2")
+		.def(py::init<float, float>())
+		.def("__repr__", [](const DirectX::XMFLOAT2& vec) {
+		using namespace std::string_literals;
+		return "["s + std::to_string(vec.x) + ","s + std::to_string(vec.y) + "]"s;
+			})
+		.def_readwrite("x", &DirectX::XMFLOAT2::x)
+		.def_readwrite("y", &DirectX::XMFLOAT2::y)
+		.def("__mul__",
+			[](DirectX::XMFLOAT2& vec1, DirectX::XMFLOAT2& vec2)
+			{
+				DirectX::XMFLOAT2 result;
+				XMStoreFloat2(&result, DirectX::XMVectorMultiply(DirectX::XMLoadFloat2(&vec1), DirectX::XMLoadFloat2(&vec2)));
+				return result;
+			}, py::is_operator())
+		.def("__add__",
+			[](DirectX::XMFLOAT2& vec1, DirectX::XMFLOAT2& vec2)
+			{
+				DirectX::XMFLOAT2 result;
+				XMStoreFloat2(&result, DirectX::XMVectorAdd(DirectX::XMLoadFloat2(&vec1), DirectX::XMLoadFloat2(&vec2)));
+				return result;
+			}, py::is_operator());
+
+
+	py::class_<DirectX::XMFLOAT4>(_module, "Vector4")
+		.def(py::init<float, float, float, float>())
+		.def("__repr__", [](const DirectX::XMFLOAT4& vec) {
+		using namespace std::string_literals;
+		return "["s + std::to_string(vec.x) + ","s + std::to_string(vec.y) + ","s + std::to_string(vec.z) + ","s + std::to_string(vec.w) + "]"s;
+			})
+		.def_readwrite("x", &DirectX::XMFLOAT4::x)
+		.def_readwrite("y", &DirectX::XMFLOAT4::y)
+		.def_readwrite("z", &DirectX::XMFLOAT4::z)
+		.def_readwrite("w", &DirectX::XMFLOAT4::w)
+		.def("__mul__",
+			[](DirectX::XMFLOAT4& vec1, DirectX::XMFLOAT4& vec2)
+			{
+				DirectX::XMFLOAT4 result;
+				XMStoreFloat4(&result, DirectX::XMVectorMultiply(DirectX::XMLoadFloat4(&vec1), DirectX::XMLoadFloat4(&vec2)));
+				return result;
+			}, py::is_operator())
+		.def("__add__",
+			[](DirectX::XMFLOAT4& vec1, DirectX::XMFLOAT4& vec2)
+			{
+				DirectX::XMFLOAT4 result;
+				XMStoreFloat4(&result, DirectX::XMVectorAdd(DirectX::XMLoadFloat4(&vec1), DirectX::XMLoadFloat4(&vec2)));
+				return result;
+			}, py::is_operator());
+#pragma endregion
+
+	py::class_<Bezier>(_module, "Bezier")
+		.def(py::init<>())
+		.def("AddControlPoint", &Bezier::AddControlPoint, "Adds control point to Bezier")
+		.def("GetXPos", &Bezier::GetXPos, "Gets X Pos")
+		.def("GetYPos", &Bezier::GetYPos, "Gets Y Pos");
 
 	py::class_<duckApp::GameObject>(_module, "GameObject")
 		.def("GetPosition", &GameObject::GetPosition, "Gets Position")
